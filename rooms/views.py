bf79395aa8b1cd797d50e85c686f3032e16f1e70
hosibito <hosibito@naver.com>
2021-12-22 22:15:35 +0900

    1.2 Serializers

--- rooms/views.py
+++ rooms/views.py
@@ -1,7 +1,12 @@
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
 
+from .models import Room as Room_models
+from .serializers import RoomSerializer
+
 
 @api_view(["GET"])
 def list_rooms(request):
-    return Response()
+    rooms = Room_models.objects.all()
+    serialized_rooms = RoomSerializer(rooms, many=True)
+    return Response(data=serialized_rooms.data)
