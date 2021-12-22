380256bff50016ccc52b7956bd6c74f68ba2d118
hosibito <hosibito@naver.com>
2021-12-22 22:46:11 +0900

    1.4 Class Based Views

--- rooms/views.py
+++ rooms/views.py
@@ -1,8 +1,30 @@
-from rest_framework.decorators import api_view
-from rest_framework.response import Response
+from rest_framework.generics import ListAPIView
+
 
 from .models import Room as Room_models
 from .serializers import RoomSerializer
+
+
+class ListRoomsView(ListAPIView):
+
+    queryset = Room_models.objects.all()
+    serializer_class = RoomSerializer
+
+
+########################################################
+from rest_framework.views import APIView
+from rest_framework.response import Response
+
+
+class _ListRoomsView(APIView):
+    def get(self, request):
+        rooms = Room_models.objects.all()
+        serialized_rooms = RoomSerializer(rooms, many=True)
+        return Response(serialized_rooms.data)
+
+
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
 
 
 @api_view(["GET"])
