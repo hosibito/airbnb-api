70c6fa811452359f018d33620179a1f6a19c73c8
hosibito <hosibito@naver.com>
2022-03-02 22:43:30 +0900

    2.4~5 Room Detail DELETE PUT

--- rooms/views.py
+++ rooms/views.py
@@ -1,3 +1,4 @@
+from tkinter.messagebox import NO
 from rest_framework.generics import ListAPIView, RetrieveAPIView
 
 from rest_framework.views import APIView
@@ -34,20 +35,49 @@ class RoomsView(APIView):
 
 
 class RoomView(APIView):
-    def get(self, request, pk):
-        print(pk)
+    def get_room(self, pk):  # pk값으로 룸모델에서 조회하는건 get,put,delete 다 사용하므로
         try:
             room = Room_models.objects.get(pk=pk)
+            return room
+        except Room_models.DoesNotExist:
+            return None
+
+    def get(self, request, pk):
+        room = self.get_room(pk)
+        if room is not None:
             serializer = ReadRoomSerializer(room).data
             return Response(serializer)
-        except Room_models.DoesNotExist:
+        else:
             return Response(status=status.HTTP_404_NOT_FOUND)
 
-    def put(self, request):
-        pass
+    def put(self, request, pk):
+        room = self.get_room(pk)
+        if room is not None:
+            if room.user != request.user:
+                return Response(status=status.HTTP_403_FORBIDDEN)
+            # room 오브젝트가 instence 이다. 이게 있으면 update로 인식하게된다.
+            serializer = WriteRoomSerializer(room, data=request.data, partial=True)
+            # print(serializer.is_valid(), serializer.errors) # True {}
+            if serializer.is_valid():
+                room = serializer.save()
+                return Response(ReadRoomSerializer(room).data)  # 200을 줘야 하는거 아닌가?
+            else:
+                return Response(
+                    data=serializer.errors, status=status.HTTP_400_BAD_REQUEST
+                )
+            return Response()
+        else:
+            return Response(status=status.HTTP_404_NOT_FOUND)
 
-    def delete(self, request):
-        pass
+    def delete(self, request, pk):
+        room = self.get_room(pk)
+        if room is not None:
+            if room.user != request.user:
+                return Response(status=status.HTTP_403_FORBIDDEN)
+            room.delete()
+            return Response(status=status.HTTP_200_OK)
+        else:
+            return Response(status=status.HTTP_404_NOT_FOUND)
 
 
 ########################################################
