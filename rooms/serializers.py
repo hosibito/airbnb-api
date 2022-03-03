f62e9f92369aafde6e29b11b02da55c1a32e133b
hosibito <hosibito@naver.com>
2022-03-03 20:13:55 +0900

    2.6~7 MeView get put

--- rooms/serializers.py
+++ rooms/serializers.py
@@ -1,19 +1,63 @@
 from rest_framework import serializers
 
-from users.serializers import UserSerializer
+from users.serializers import RelatedUserSerializer
 from .models import Room as Room_models
 
 
 class ReadRoomSerializer(serializers.ModelSerializer):
 
-    user = UserSerializer()
+    user = RelatedUserSerializer()
 
     class Meta:
         model = Room_models
         exclude = ("modified",)
 
 
-class WriteRoomSerializer(serializers.Serializer):
+class WriteRoomSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Room_models
+        exclude = ("user", "modified", "created")
+
+    def validate_beds(self, beds):
+        print(beds)
+        if beds < 5:
+            raise serializers.ValidationError("Your house is too small")
+        return beds
+
+    def validate(self, data):
+        print(data)  # OrderedDict([('name', 'posttest222')])
+        if self.instance:
+            check_in = data.get("check_in", self.instance.check_in)
+            check_out = data.get("check_out", self.instance.check_out)
+        else:
+            check_in = data.get("check_in")
+            check_out = data.get("check_out")
+
+        if check_in == check_out:
+            raise serializers.ValidationError("Not enough time between changes")
+
+        return data
+
+
+#########################################################
+class _RoomSerializer(serializers.Serializer):
+    name = serializers.CharField(max_length=140)
+    price = serializers.IntegerField()
+    bedrooms = serializers.IntegerField()
+    instant_book = serializers.BooleanField()
+
+
+class __RoomSerializer(serializers.ModelSerializer):
+
+    user = RelatedUserSerializer()
+
+    class Meta:
+        model = Room_models
+        # fields = ("pk", "name", "price", "instant_book", "user") # 표시할것을 명기
+        exclude = ("modified",)  # 뺄것을 명기
+
+
+class __WriteRoomSerializer(serializers.Serializer):
 
     name = serializers.CharField(max_length=140)
     address = serializers.CharField(max_length=140)
@@ -72,21 +116,3 @@ class WriteRoomSerializer(serializers.Serializer):
         )
         instance.save()
         return instance
-
-
-#########################################################
-class _RoomSerializer(serializers.Serializer):
-    name = serializers.CharField(max_length=140)
-    price = serializers.IntegerField()
-    bedrooms = serializers.IntegerField()
-    instant_book = serializers.BooleanField()
-
-
-class __RoomSerializer(serializers.ModelSerializer):
-
-    user = UserSerializer()
-
-    class Meta:
-        model = Room_models
-        # fields = ("pk", "name", "price", "instant_book", "user") # 표시할것을 명기
-        exclude = ("modified",)  # 뺄것을 명기
--- users/serializers.py
+++ users/serializers.py
@@ -2,18 +2,35 @@ from rest_framework import serializers
 from .models import User as User_madels
 
 
-class UserSerializer(serializers.ModelSerializer):
+class RelatedUserSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = User_madels
+        fields = (
+            "username",
+            "first_name",
+            "last_name",
+            "email",
+            "avatar",
+            "superhost",
+        )
+
+
+class ReadUserSerializer(serializers.ModelSerializer):
     class Meta:
         model = User_madels
         # fields = ("username", "superhost")  # 표시할것을 명기
         exclude = (  # 뺄것을 명기
-            "groups",
-            "user_permissions",
-            "password",
-            "last_login",
-            "is_superuser",
             "is_staff",
             "is_active",
             "date_joined",
-            "favs",
         )
+
+
+class WriteUserSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = User_madels
+        fields = ("username", "first_name", "last_name", "email")
+
+    def validate_first_name(self, value):
+        print(value)
+        return value.upper()
