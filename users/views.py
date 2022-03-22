b99104a2516c10c702b2eba9edc4bc2afd1f37a5
hosibito <hosibito@naver.com>
2022-03-22 14:02:48 +0900

    2.11 Log In (JWT)

--- users/views.py
+++ users/views.py
@@ -1,3 +1,6 @@
+import jwt
+from django.conf import settings  # import settings  는 절대 금지!
+from django.contrib.auth import authenticate
 from rest_framework.response import Response
 from rest_framework.permissions import IsAuthenticated
 from rest_framework.decorators import api_view
@@ -17,6 +20,22 @@ class UsersView(APIView):
             return Response(UserSerializer(new_user).data)
         else:
             return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+
+@api_view(["POST"])
+def login(request):
+    username = request.data.get("username")
+    password = request.data.get("password")
+    if not username or not password:
+        return Response(status=status.HTTP_400_BAD_REQUEST)
+    user = authenticate(username=username, password=password)
+    if user is not None:
+        encoded_jwt = jwt.encode(
+            {"pk": user.pk}, settings.SECRET_KEY, algorithm="HS256"
+        )
+        return Response(data={"token": encoded_jwt})
+    else:
+        return Response(status=status.HTTP_401_UNAUTHORIZED)
 
 
 class MeView(APIView):
